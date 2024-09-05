887d87ecedf496e81ecf619c3450b7f17f349f9c
arlo <arlovalles@yahoo.com>
2024-09-05 11:54:09 -0700

    some cleanup. Standardize the entry point in a main function.

--- checkMyPass.py
+++ checkMyPass.py
@@ -10,20 +10,18 @@ def request_api_data(query_char):
     response = requests.get(f"{url}/{query_char}")
     if response.status_code != 200:
         raise RuntimeError(f"Error Retrieving: {response.status_code} - {query_char}")
-    #print(response)
-    #print(response.content)
     return response    
 
-def get_password_leaks_count(hashes, hash_to_check):
+def get_password_leaks_count(hashes, hash_to_check)->int:
     '''
     Extract Count for Password hash that matches in the results
     '''
     result = 0
-    for hash in hashes:
-        dta = hash.split(':')
-        if dta[0] == hash_to_check:
-            #print(dta)
-            result = dta[1]
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for hash, count in hashes:
+        if hash == hash_to_check:
+            result = int(count)
+            break
     return result        
 
 def pwned_api_check(password:str='', encoding:str='utf-8'):    
@@ -33,13 +31,24 @@ def pwned_api_check(password:str='', encoding:str='utf-8'):
     sha1_password = hashlib.sha1(password.encode(encoding)).hexdigest().upper()
     first5Chars, remainingChars = sha1_password[:5], sha1_password[5:] 
     response = request_api_data(first5Chars)    
-    matchCount = get_password_leaks_count(response.text.splitlines(), remainingChars)
-    print(f"{password} has been breached {matchCount} times.") 
+    return get_password_leaks_count(response, remainingChars)
+
+
+def main(args):
+    '''
+    Process an array of password arguments
+    '''
+    for pwd in args:
+        count = pwned_api_check(password=pwd)
+        if count > 0:
+            print(f"{pwd} has been breached {count} times.") 
+        else:
+            print(f"{pwd} was not found.") 
+    return
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        for arg in sys.argv[1:]:
-            pwned_api_check(password=arg)
+        main(sys.argv[1:])
     else:
         print("Running in Demo Mode.")
         pwned_api_check(password='password1234')
